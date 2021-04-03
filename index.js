0fb121e25508a891c126524034dc27d6e09fe33f
Love Mishra <>
2021-04-03 06:12:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// var readlineSync = require('readline-sync');
+ 
+
+// var userName = readlineSync.question('May I have your name? ');
+// console.log('Hi ' + userName + '!');
+
+var r=require('readline-sync');
+var user=r.question("Hello, your name please ??");
+console.log("Welcome !"+user);
