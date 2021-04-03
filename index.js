d16f1abe1de6f7868189d730dce8efa1c634d6f2
Love Mishra <>
2021-04-03 07:48:54 +0000

    In this program I has printed my Name five times and number is appended in the last.

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-var readline=require("readline-sync");
-var score;
-function play(question,answer){
-
-  score=0;
-responce=readline.question(question);
-
-if (responce==answer){
-// console.log("VOILA !!! You are correct!");
-score++;
-
-
-}else{
-
-  score--;
-}
-return score;
+for (var i=1;i<=5;i++){
+console.log("Love Mishra !!!"+i);
 
 }
-var a=play("What is his full name? ","Love Mishra");
-var b=play("What is his age? ","22");
-var c=play("What does he want to become ? ","Full Stack Developer");
-
-
-
-console.log( "Your Score is :"+""+(a+b+c));
