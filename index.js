c43592a7e48b5b2c7c52c61d0f2a3f1eafff11c8
Love Mishra <>
2021-04-03 08:13:50 +0000

    Wrong pattern drawn. Posted my question on stack overflow and waiting for someone to help.
    quesion url:https://stackoverflow.com/questions/66929007/node-js-pattern

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-for (var i=1;i<=5;i++){
-console.log("Love Mishra !!!"+i);
+var readline=require("readline-sync");
+var n=parseInt(readline.question());
+
+for (var i=1;i<=n;i++){
+  var str="";
+for (var j=1;j<=i;j++){
+str+="*";
+  console.log(str);
+
+}console.log();
+str="";
 
 }
+
+console.log(n);
