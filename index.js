9efcf7236eb98de568e6c39d02b90ddd75b267a3
Love Mishra <>
2021-04-07 14:24:39 +0000

    I created an array of an object and then traversed it.

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ var supermen={
 };
 var batmen={
   height:120,
-  name:"I am a Disko Dancer ! Jindgi mera Gana",
+  name:"I am the Brother of Padmen !!!",
   color:"black",
   strenth:20
 };
-console.log("Color of two SuperPowers is:")
-console.log(supermen.color+" " +batmen.color);
-console.log(supermen.name);
-console.log(batmen.name);
-console.log(batmen.strenth>supermen.strenth);
+// console.log("Color of two SuperPowers is:")
+// console.log(supermen.color+" " +batmen.color);
+// console.log(supermen.name);
+// console.log(batmen.name);
+// console.log(batmen.strenth>supermen.strenth);
+
+var herolist=[batmen,supermen];
+
+for (var i=0;i<herolist.length;i++){
+  console.log(herolist[i].name);
+  console.log("My color is :"+herolist[i].color);
+  console.log("*******************")
+}
